a24c7e62a75e3f88a88d07668d87fd6cfc72e6e4
lewishta <79660599+lewishta@users.noreply.github.com>
2021-08-12 22:05:19 +0100

    this  is broken

--- R/02_calibration-functions_neilder.R
+++ R/02_calibration-functions_neilder.R
@@ -116,7 +116,7 @@ v_params_test <- c(p_N1N2_6 = 0.2,
                    p_N2D_24 = 0.05,
                    p_N2D_30 = 0.05,
                    p_N2D_36 = 0.05
-                   )
+)
 
 
 test_results <- run_sick_sicker_markov(v_params_test) # It works!
@@ -172,8 +172,8 @@ legend("bottomright", legend = c("Targets", "Outputs"),
 # Specify seed (for reproducible sequence of random numbers)
 set.seed(072218)
 
-# number of random samples
-n_samp <- 100
+# number of initial starting points
+n_init <- 1
 
 # names and number of input parameters to be calibrated
 v_param_names <- c("p_N1N2_6",
@@ -187,18 +187,17 @@ v_param_names <- c("p_N1N2_6",
                    "p_N2N3_6",
                    "p_N2N3_12",
                    "p_N2N3_18",
-                  
+                   
                    "p_N2D_6", 
                    "p_N2D_12",
                    "p_N2D_18"
                    
 )
-  
+
 n_param <- length(v_param_names)
 
-# range on input search space
-#lb <- c(p_Mets = 0.04, p_DieMets = 0.04) # lower bound
-#ub <- c(p_Mets = 0.16, p_DieMets = 0.16) # upper bound
+
+#range on input search space
 
 lb <- c(p_N1N2_6 = 0.04,
         p_N1N2_12 = 0.04,
@@ -218,28 +217,28 @@ lb <- c(p_N1N2_6 = 0.04,
         p_N2D_6 = 0.04,
         p_N2D_12 = 0.04,
         p_N2D_18 = 0.04
-       
+        
 )
 
 ub <- c(p_N1N2_6 = 0.2,
         p_N1N2_12 = 0.2,
         p_N1N2_18 = 0.2,
-       
+        
         
         p_N1D_6 = 0.2,
         p_N1D_12 = 0.2,
         p_N1D_18 = 0.2,
         
         
-        p_N2N3_6 = 0.2,
-        p_N2N3_12 = 0.2,
-        p_N2N3_18 = 0.2,
-       
+        p_N2N3_6 = 0.02,
+        p_N2N3_12 = 0.02,
+        p_N2N3_18 = 0.02,
+        
         
         p_N2D_6 = 0.2,
         p_N2D_12 = 0.2,
         p_N2D_18 = 0.2
-       
+        
 )
 
 
@@ -250,11 +249,112 @@ v_target_names <- c("prop_N1", "prop_N2", "prop_N1" )
 n_target <- length(v_target_names)
 
 
+### Calibration functions
+
+# Write goodness-of-fit function to pass to Nelder-Mead algorithm
+f_gof <- function(v_params){
+
+  # Run model for parametr set "v_params"
+  model_res <- run_sick_sicker_markov(v_params)
+  
+  # Calculate goodness-of-fit of model outputs to targets
+  v_GOF <- numeric(n_target)
+  
+  # TARGET 1: N1
+  # log likelihood  
+  v_GOF[1] <- sum(dnorm(x = lst_targets$dist$N1$value,
+                        mean = model_res$prop_N1_use,
+                        sd = lst_targets$dist$N1$se,
+                        log = T))
+  
+  # TARGET 1: N2
+  # log likelihood  
+  v_GOF[1] <- sum(dnorm(x = lst_targets$dist$N2$value,
+                        mean = model_res$prop_N2_use,
+                        sd = lst_targets$dist$N2$se,
+                        log = T))
+  
+  # TARGET 1: N3
+  # log likelihood  
+  v_GOF[1] <- sum(dnorm(x = lst_targets$dist$N3$value,
+                        mean = model_res$prop_N3_use,
+                        sd = lst_targets$dist$N3$se,
+                        log = T))
+  
+  # OVERALL
+  # can give different targets different weights
+  v_weights <- rep(1,n_target)
+  # weighted sum
+  GOF_overall <- sum(v_GOF[1:n_target] * v_weights)
+  
+  # return GOF
+  return(GOF_overall)
+}
+
+
+
+
 ####################################################################
 ######  Calibrate!  ######
 ####################################################################
 # record start time of calibration
 t_init <- Sys.time()
+
+###  Sample multiple random starting values for Nelder-Mead  ###
+v_params_init <- matrix(nrow=n_init,ncol=n_param)
+for (i in 1:n_param){
+  v_params_init[,i] <- runif(n_init,min=lb[i],max=ub[i])
+}
+colnames(v_params_init) <- v_param_names
+
+head(v_params_init)
+
+###  Run Nelder-Mead for each starting point  ###
+m_calib_res <- matrix(nrow = n_init, ncol = n_param+1)
+colnames(m_calib_res) <- c(v_param_names, "Overall_fit")
+for (j in 1:n_init){
+
+  ### use optim() as Nelder-Mead ###
+  fit_nm <- optim(v_params_init[j,], f_gof,
+                  control = list(fnscale = -1, # switches from minimization to maximization
+                                 maxit = 1000), hessian = T)
+  m_calib_res[j,] <- c(fit_nm$par,fit_nm$value)
+
+
+}
+  # Calculate computation time
+  comp_time <- Sys.time() - t_init
+
+  ###################################################################
+  ######  Exploring best-fitting input sets  ######
+  ####################################################################
+  
+  # Arrange parameter sets in order of fit
+  m_calib_res <- m_calib_res[order(-m_calib_res[,"Overall_fit"]),]
+  
+  # Examine the top 10 best-fitting sets
+  m_calib_res[1:10,]
+  
+  # Plot the top 10 (top 10%)
+  plot(m_calib_res[1:10,1],m_calib_res[1:10,2],
+       xlim=c(lb[1],ub[1]),ylim=c(lb[2],ub[2]),
+       xlab = colnames(m_calib_res)[1],ylab = colnames(m_calib_res)[2])
+  
+  # Pairwise comparison of top 10 sets
+  pairs.panels(m_calib_res[1:10,v_param_names])
+  
+  
+  ### Plot model-predicted output at mean vs targets ###
+  v_out_best <- run_crs_markov(m_calib_res[1,])
+
+
+
+
+
+
+
+
+
 
 ###  Generate a random sample of input values  ###
 
