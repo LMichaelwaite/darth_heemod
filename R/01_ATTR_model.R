9f5740981109e03dd834b588fe2a16745f995a4e
lewishta <79660599+lewishta@users.noreply.github.com>
2021-08-31 14:41:23 +0100

    aa

--- R/01_ATTR_model.R
+++ R/01_ATTR_model.R
@@ -91,19 +91,23 @@ param <- define_parameters(
   
   disutility_hosp = 0.10, # https://onlinelibrary.wiley.com/doi/full/10.1002/ehf2.12844
   
-  #### COSTS AND UTILITIES ####                                         
+  #### COSTS AND UTILITIES ####  
+  
+  # EXPERT ELLICITATION: weighting for NYHA to NAC
+  # NAC: 
   #divde by two due to 6 month cycle
   u_NAC_I = 0.893/2, # Source: Rozenbaum NYHA utilities. Assume NYHA I = NAC I,..., AVG(NYHA_III, NYHA_IV) = NAC_III
   u_NAC_II = 0.802/2,
-  u_NAC_III = ((0.706 + 0.406)/2)/2, 
+  u_NAC_III = (0.7*0.706+0.3*0.406)/2, 
   u_NAC_I_tafa = 0.874/2,
   u_NAC_II_tafa = 0.832/2,
-  u_NAC_III_tafa = ((0.707 + 0.558)/2)/2,
+  u_NAC_III_tafa = (0.7*0.707 + 0.3*0.558)/2,
   c_tafa = 10840.82*6, # cost per 6 months; GBP; source: NICE tafamidis STA pp. 137
   c_bsc = 7031.92/2, # GBP; source nuffield trust estimate for health care sepnding on 75 year olds (UK) inflation adjusted to 2020 ((find actual source)) https://www.theguardian.com/society/2016/feb/01/ageing-britain-two-fifths-nhs-budget-spent-over-65s 
   #c_bsc = 18462/2, # background hc cost 6 months; dollars US Kazi
   c_hosp = 2536.88, # GBP source: NICE tafamidis STA pp.140
-  c_eol = 9287.86 # # GBP source: NICE tafamidis STA pp.140, end of life costs
+  c_eol = 9287.86, # # GBP source: NICE tafamidis STA pp.140, end of life costs
+  R = 0.03  #discount rate
 )
 #-------------------------------------------------------------------------------
 #                       DEFINE TRANSITION MATRIX
@@ -140,56 +144,60 @@ plot(mat_tafa) #model diagram
 #-------------------------------------------------------------------------------
 #           DEFINE strategy & state dependent costs and utilities
 #...............................................................................
+R <- (1+0.03)^(1/3) - 1
 NAC1 <- define_state(
-  cost_background_health = c_bsc,
-  cost_drugs = dispatch_strategy(
-    bsc = 0,
-    tafa = c_tafa
-  ),
-  cost_hosp = dispatch_strategy(
-    bsc = p_hosp * c_hosp,
-    tafa = p_hosp_tafa* c_hosp),
-  cost_total = discount(cost_background_health + cost_drugs + cost_hosp, .03),
+  cost_background_health = discount(c_bsc, R),
+  cost_drugs = discount(dispatch_strategy(
+      bsc = 0,
+      tafa = c_tafa, 
+  ), R),
+  cost_hosp = discount(dispatch_strategy(
+      bsc = p_hosp * c_hosp,
+      tafa = p_hosp_tafa* c_hosp), R),
+  cost_total = discount(cost_background_health + cost_drugs + cost_hosp, 0),
+  
   qaly = dispatch_strategy(
-    bsc = u_NAC_I,
-    tafa = u_NAC_I_tafa
-    ),
+      bsc = u_NAC_I,
+      tafa = u_NAC_I_tafa
+      ),
   cv_hosp_disutility = dispatch_strategy(
-    bsc = p_hosp * disutility_hosp,
-    tafa = p_hosp_tafa* disutility_hosp),
-  qaly_total =  discount(qaly-cv_hosp_disutility, 0.0),
-  life_year = 0.5
+      bsc = p_hosp * disutility_hosp,
+      tafa = p_hosp_tafa* disutility_hosp),
+  qaly_total =  discount(qaly-cv_hosp_disutility, R),
+  life_year = discount(0.5, R)
 )
 NAC2 <- define_state(
-  cost_background_health = c_bsc,
-  cost_drugs = dispatch_strategy(
-    bsc = 0,
-    tafa = c_tafa
-  ),
-  cost_hosp = dispatch_strategy(
-    bsc = p_hosp * c_hosp,
-    tafa = p_hosp_tafa* c_hosp),
-  cost_total = discount(cost_background_health + cost_drugs + cost_hosp, .03),
+  cost_background_health = discount(c_bsc, R),
+  cost_drugs = discount(dispatch_strategy(
+        bsc = 0,
+        tafa = c_tafa, 
+      ), R),
+  cost_hosp = discount(dispatch_strategy(
+      bsc = p_hosp * c_hosp,
+      tafa = p_hosp_tafa* c_hosp), R),
+  cost_total = discount(cost_background_health + cost_drugs + cost_hosp, 0),
+  
   qaly = dispatch_strategy(
-    bsc = u_NAC_II,
-    tafa = u_NAC_II_tafa
+      bsc = u_NAC_II,
+      tafa = u_NAC_II_tafa
   ),
   cv_hosp_disutility = dispatch_strategy(
-    bsc = p_hosp * disutility_hosp,
-    tafa = p_hosp_tafa* disutility_hosp),
-  qaly_total =  discount(qaly- cv_hosp_disutility, 0.0),
-  life_year = 0.5
+      bsc = p_hosp * disutility_hosp,
+      tafa = p_hosp_tafa* disutility_hosp),
+  qaly_total =  discount(qaly- cv_hosp_disutility, R),
+  life_year = discount(0.5, R)
 )
 NAC3 <- define_state(
-  cost_background_health = c_bsc,
-  cost_drugs = dispatch_strategy(
+  cost_background_health = discount(c_bsc, R),
+  cost_drugs = discount(dispatch_strategy(
     bsc = 0,
-    tafa = c_tafa
-  ),
-  cost_hosp = dispatch_strategy(
+    tafa = c_tafa, 
+  ), R),
+  cost_hosp = discount(dispatch_strategy(
     bsc = p_hosp * c_hosp,
-    tafa = p_hosp_tafa* c_hosp),
-  cost_total = discount(cost_background_health + cost_drugs + cost_hosp, .03),
+    tafa = p_hosp_tafa* c_hosp), R),
+  cost_total = discount(cost_background_health + cost_drugs + cost_hosp, 0),
+
   qaly = dispatch_strategy(
     bsc = u_NAC_III,
     tafa = u_NAC_III_tafa
@@ -197,8 +205,8 @@ NAC3 <- define_state(
   cv_hosp_disutility = dispatch_strategy(
     bsc = p_hosp * disutility_hosp,
     tafa = p_hosp_tafa* disutility_hosp),
-  qaly_total =  discount(qaly - cv_hosp_disutility, 0.0),
-  life_year = 0.5
+  qaly_total =  discount(qaly - cv_hosp_disutility, R),
+  life_year = discount(0.5, R)
 )
 Death <- define_state(
   cost_background_health = 0,
@@ -207,8 +215,8 @@ Death <- define_state(
   cost_total = discount(cost_background_health + cost_drugs + cost_hosp, .03),
   qaly = 0,
   cv_hosp_disutility = 0,
-  qaly_total =  discount(qaly - cv_hosp_disutility, 0.0),
-  life_year = 0
+  qaly_total =  discount(qaly - cv_hosp_disutility, R),
+  life_year = discount(0, R)
 )
 strat_bsc <- define_strategy(
   transition = mat_bsc,
@@ -228,7 +236,7 @@ res_mod <- run_model(
   bsc = strat_bsc,
   tafa = strat_tafa,
   parameters = param,
-  cycles = 21,
+  cycles = 40,
   cost = cost_total,
   #effect = qaly_total,
   effect = life_year,
@@ -284,22 +292,26 @@ rsp <- define_psa(
   
   disutility_hosp ~ beta(10, 90), # source: https://onlinelibrary.wiley.com/doi/full/10.1002/ehf2.12844
   
-  p_N1D_1 ~ beta(1.201171875, 434.7988281),   # alpha = count of events; beta = number at risk - alpha 
-  p_N2D_1 ~ beta(1.880274781, 348.1197252),
-  p_N3D_1 ~ beta(1.918624122, 157.0813759),
+  p_N1N2_1 ~ beta(param$p_N1N2_1, param$p_N1N2_1*0.15),
+  p_N2N3_1 ~ beta(param$p_N2N3_1, param$p_N2N3_1*0.15),
+  p_N1N2_2 ~ beta(param$p_N1N2_2, param$p_N1N2_2*0.15),
+  p_N2N3_2 ~ beta(param$p_N2N3_2, param$p_N2N3_2*0.15),
+  p_N1N2_3 ~ beta(param$p_N1N2_3, param$p_N1N2_3*0.15),
+  p_N2N3_3 ~ beta(param$p_N2N3_3, param$p_N2N3_3*0.15),
   
-  p_N1D_2 ~ beta(4.4792, 181.5208),
-  p_N2D_2 ~ beta(8.6731, 138.3269),
-  p_N3D_2 ~ beta(10.8477, 88.1523),
-  
-  p_N1D_3 ~ beta(11.112, 204.888),
-  p_N2D_3 ~ beta(26.594, 184.406),
-  p_N3D_3 ~ beta(29.794, 105.206),
-  
-  p_N1D_4 ~ beta(9.997, 191.373),
-  p_N2D_4 ~ beta(25.703, 185.617),
-  p_N3D_4 ~ beta(31.800, 117.510),
-  
+  p_N1D_1 ~ beta(param$p_N1D_1, param$p_N1D_1*0.15),   # alpha = count of events; beta = number at risk - alpha 
+  p_N2D_1 ~ beta(param$p_N2D_1, param$p_N2D_1*0.15),
+  p_N3D_1 ~ beta(param$p_N3D_1, param$p_N3D_1*0.15),
+  p_N1D_2 ~ beta(param$p_N1D_2, param$p_N1D_2*0.15),   
+  p_N2D_2 ~ beta(param$p_N2D_2, param$p_N2D_2*0.15),
+  p_N3D_2 ~ beta(param$p_N3D_2, param$p_N3D_2*0.15),
+  p_N1D_3 ~ beta(param$p_N1D_3, param$p_N1D_3*0.15),   
+  p_N2D_3 ~ beta(param$p_N2D_3, param$p_N2D_3*0.15),
+  p_N3D_3 ~ beta(param$p_N3D_3, param$p_N3D_3*0.15),
+  p_N1D_4 ~ beta(param$p_N1D_4, param$p_N1D_4*0.15),   
+  p_N2D_4 ~ beta(param$p_N2D_4, param$p_N2D_4*0.15),
+  p_N3D_4 ~ beta(param$p_N3D_4, param$p_N3D_4*0.15),
+ 
   c_bsc ~ gamma(mean = 7031.92/2, sd = sqrt((7031.92/2))), # "gamma" seems to not require the above gamma function. ?
   c_hosp ~ gamma(mean = 2546, sd= 507.38),
   
@@ -480,21 +492,55 @@ df_values_tafa <- df_values %>% # Tafa
 values_bsc_wide <- pivot_wider(df_values_bsc, names_from = value_names, values_from = value)
 values_tafa_wide <- pivot_wider(df_values_tafa, names_from = value_names, values_from = value)
 
-#### create column: cohort size per cycle
-#c_alive_bsc <-c_state_wide %>%
+## n prop
+#nac_dist <-c_state_wide %>%
 #  rowwise() %>%
 #  mutate(total_alive = sum(across(starts_with("NAC")), na.rm = T)) %>%
+#  mutate(proportion_alive = total_alive/rowSums(across(Death:total_alive)))%>%
+#  mutate(proportion_N1= NAC1/total_alive)%>%
+#  mutate(proportion_N2= NAC2/total_alive)%>%
+#  mutate(proportion_N3= NAC3/total_alive)%>%
 #  filter(.strategy_names =="bsc")%>%
-#  select(total_alive)
+#  select(total_alive, proportion_N1,proportion_N2, proportion_N3)
 #
-#c_alive_tafa <-c_state_wide %>%
+#
+#values_bsc_1 <- cbind(values_bsc_wide, nac_dist)
+#
+#
+#cal_tar <- values_bsc_1  %>%
 #  rowwise() %>%
-#  mutate(total_alive = sum(across(starts_with("NAC")), na.rm = T)) %>%
-#  filter(.strategy_names =="tafa")%>%
-#  select(total_alive)
+#  mutate(targ = qaly_total/life_year) %>%
+#  filter(markov_cycle %in% c(2, 20)) %>%
+#  select(targ)
 #
-#values_bsc <- cbind(values_bsc_wide, c_alive_bsc)
-#values_tafa <- cbind(values_tafa_wide, c_alive_tafa)        
+#z <- values_bsc_1  %>%
+#  rowwise() %>%
+#  mutate(targ = qaly_total/life_year) %>%
+#  
+#  select(targ)  
+#
+#
+#qaly_check <- values_bsc_1 %>%
+#  rowwise() %>%
+#  mutate(qaly_1 = qaly/total_alive) %>%
+#  select(markov_cycle, qaly_1)
+#
+
+#### create column: cohort size per cycle
+c_alive_bsc <-c_state_wide %>%
+  rowwise() %>%
+  mutate(total_alive = sum(across(starts_with("NAC")), na.rm = T)) %>%
+  filter(.strategy_names =="bsc")%>%
+  select(total_alive)
+
+c_alive_tafa <-c_state_wide %>%
+  rowwise() %>%
+  mutate(total_alive = sum(across(starts_with("NAC")), na.rm = T)) %>%
+  filter(.strategy_names =="tafa")%>%
+  select(total_alive)
+
+values_bsc <- cbind(values_bsc_wide, c_alive_bsc)
+values_tafa <- cbind(values_tafa_wide, c_alive_tafa)        
         
 #### Calculate mean values ####        
 avg_qaly_bsc <-  sum(values_bsc_wide$qaly_total)/c_n        
@@ -509,6 +555,10 @@ avg_cost_hosp_tafa <- sum(values_tafa_wide$cost_hosp)/c_n
 
 avg_drug_cost_tafa <- sum(values_tafa_wide$cost_drugs)/c_n
 
+incremental_cost <- avg_cost_tafa - avg_cost_bsc
+incremental_ly <- avg_ly_tafa - avg_ly_bsc
+incremental_qaly <- avg_qaly_tafa - avg_qaly_bsc
+
 result_summary <- data.frame(QALY_tafa =  avg_qaly_tafa,
                              QALY_bsc =  avg_qaly_bsc,
                              Life_years_tafa = avg_ly_tafa,
@@ -517,10 +567,73 @@ result_summary <- data.frame(QALY_tafa =  avg_qaly_tafa,
                              cost_bsc = avg_cost_bsc,
                              drug_cost_tafa = avg_drug_cost_tafa,
                              hosp_cost_tafa = avg_cost_hosp_tafa,
-                             hosp_cost_bsc = avg_cost_hosp_bsc )       
+                             hosp_cost_bsc = avg_cost_hosp_bsc,
+                             incremental_cost = incremental_cost,
+                             incremental_ly = incremental_ly,
+                             incremental_qaly = incremental_qaly)       
 result_summary        
         
-        
+#----------------------------------------------------------
+#plot survival stuff
+#-------------------------------------------------------------
+#plot death state count
+plot(res_mod, type = "counts", states = "Death", panel = "by_state", free_y = TRUE) +
+  theme_bw() +
+  scale_color_brewer(
+    name = "Strategy",
+    palette = "Set1"
+  )
+
+#get state counts
+c_state <- as.data.frame(get_counts(res_mod))
+
+c_state_bsc <- filter(c_state, .strategy_names == "bsc")
+c_state_tafamidis <- filter(c_state, .strategy_names == "tafamidis")
+
+
+c_state_wide_2 <- pivot_wider(c_state_bsc, names_from = state_names, values_from = count)
+
+#### BSC survival ####
+#convert long table into wide table
+c_state_wide <- pivot_wider(c_state, names_from = c(state_names,.strategy_names), values_from = count) 
+
+#create new columns: total_alive & proportion_alive
+surv_prop <-c_state_wide %>%
+  rowwise() %>%
+  mutate(total_alive_tafa = sum(across(c(NAC1_tafa, NAC2_tafa, NAC3_tafa)), na.rm = T)) %>%
+  mutate(total_dead_tafa = Death_tafa) %>%
+  mutate(proportion_alive = total_alive_tafa/rowSums(across(Death_tafa:total_alive_tafa)))%>%
+  select(markov_cycle, proportion_alive)
+surv_prop
+
+surv_prop_bsc <- c_state_wide %>%
+  rowwise() %>%
+  mutate(total_alive_bsc = sum(across(c(NAC1_bsc, NAC2_bsc, NAC3_bsc)), na.rm = T)) %>%
+  mutate(proportion_alive = total_alive_bsc/rowSums(across(Death_bsc:total_alive_bsc))) %>%
+  mutate(total_dead_bsc = Death_bsc) %>%  
+  select(markov_cycle, proportion_alive, total_alive_bsc, total_dead_bsc)
+surv_prop_bsc 
+
+
+# Kazi et al
+kazi_surv_tafa<- read.csv("data_raw/kazi_tafa.csv", header = FALSE)
+kazi_x_tafa<-kazi_surv_tafa[,1]/6
+kazi_y_tafa<-kazi_surv_tafa[,2]
+kazi_tafa <- data.frame(kazi_x = kazi_x_tafa, kazi_y = kazi_y_tafa)
+
+kazi_surv_bsc<- read.csv("data_raw/kazi_control.csv", header = FALSE)
+kazi_x_bsc<-kazi_surv_bsc[,1]/6
+kazi_y_bsc<-kazi_surv_bsc[,2]
+kazi_bsc <- data.frame(kazi_x = kazi_x_bsc, kazi_y = kazi_y_bsc)
+
+
+
+#plot surv_prop curve
+ggplot() + 
+  geom_line(data=surv_prop, aes(x=markov_cycle, y=proportion_alive), color='green') + 
+  geom_line(data=surv_prop_bsc, aes(x=markov_cycle, y=proportion_alive), color='red') +
+  geom_line(data=kazi_tafa, aes(x=kazi_x, y=kazi_y), color='blue') +
+  geom_line(data=kazi_bsc, aes(x=kazi_x, y=kazi_y), color='orange') 
         
         
         
